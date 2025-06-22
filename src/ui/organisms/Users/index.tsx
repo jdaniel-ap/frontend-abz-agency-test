42c22bef44b3d9ea4acaa3df84a34b6110673a57
Jose Daniel Arreaza <jdaniell.93@gmail.com>
2025-06-22 18:46:40 -0300

    feat: update Users component structure and styles for improved layout ✨

--- src/ui/organisms/Users/index.tsx
+++ src/ui/organisms/Users/index.tsx
@@ -1,5 +1,11 @@
+import { Heading } from "@/ui/atoms";
+import styles from "./Users.module.scss";
 function Users() {
-  return <div>Users</div>;
+  return (
+    <section className={styles.container}>
+      <Heading>Working with GET request</Heading>
+    </section>
+  );
 }
 
 export default Users;
