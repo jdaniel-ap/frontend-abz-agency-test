e3557000671ff27f0c8fe9417c855259cf9ac2dc
Jose Daniel Arreaza <jdaniell.93@gmail.com>
2025-06-22 18:27:45 -0300

    feat: enhance hero image with loading effct ✨

--- src/ui/molecules/Hero/index.tsx
+++ src/ui/molecules/Hero/index.tsx
@@ -1,10 +1,23 @@
-import { Heading, Text } from "@/ui/atoms";
+import { useLayoutEffect, useState } from "react";
+import { Button, Heading, Text } from "@/ui/atoms";
 import styles from "./Hero.module.scss";
 
 function Hero() {
+  const [imageLoaded, setImageLoaded] = useState(false);
+
+  useLayoutEffect(() => {
+    const img = new Image();
+    img.onload = () => {
+      setImageLoaded(true);
+    };
+    img.src = "/src/assets/pexels-alexandr-podvalny-1227513-hd.jpeg";
+  }, []);
+
   return (
-    <section className={styles.hero}>
-      <div className={styles.content}>
+    <section
+      className={`${styles.hero} ${imageLoaded ? styles["hero--loaded"] : ""}`}
+    >
+      <div className={styles.hero__content}>
         <Heading variant="light">
           Test assignment for front-end developer
         </Heading>
@@ -15,6 +28,7 @@ function Hero() {
           mind. They should also be excited to learn, as the world of Front-End
           Development keeps evolving.
         </Text>
+        <Button>Sign up</Button>
       </div>
     </section>
   );
