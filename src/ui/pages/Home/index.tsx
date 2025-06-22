c30b6720c59658f51870960a5f48e1449f181a76
Jose Daniel Arreaza <jdaniell.93@gmail.com>
2025-06-22 18:38:40 -0300

    feat: add Users component and integrate it into Home page ✨

--- src/ui/pages/Home/index.tsx
+++ src/ui/pages/Home/index.tsx
@@ -1,10 +1,12 @@
 import { Header, Hero } from "@/ui/molecules";
+import { Users } from "@/ui/organisms";
 
 function Home() {
   return (
     <>
       <Header />
       <Hero />
+      <Users />
     </>
   );
 }
