0bcce1fb7f4febba2fc1b7af81de2428d16371ef
Jose Daniel Arreaza <jdaniell.93@gmail.com>
2025-06-22 17:16:13 -0300

    feat: add hero content ✨

--- src/ui/molecules/Hero/index.tsx
+++ src/ui/molecules/Hero/index.tsx
@@ -1,7 +1,23 @@
+import { Heading, Text } from "@/ui/atoms";
 import styles from "./Hero.module.scss";
 
 function Hero() {
-  return <section className={styles.hero}></section>;
+  return (
+    <section className={styles.hero}>
+      <div className={styles.content}>
+        <Heading variant="light">
+          Test assignment for front-end developer
+        </Heading>
+        <Text variant="light">
+          What defines a good front-end developer is one that has skilled
+          knowledge of HTML, CSS, JS with a vast understanding of User design
+          thinking as they'll be building web interfaces with accessibility in
+          mind. They should also be excited to learn, as the world of Front-End
+          Development keeps evolving.
+        </Text>
+      </div>
+    </section>
+  );
 }
 
 export default Hero;
