8e4c66228d310dbf604cfbb92ec08a75a4a2f826
Jose Daniel Arreaza <jdaniell.93@gmail.com>
2025-06-22 20:23:04 -0300

    feat: add @tanstack/react-query for data fetching and wrap App component with QueryClientProvider ✨

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Home from "./ui/pages/Home";
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 2,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
 function App() {
-  return <Home />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Home />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
