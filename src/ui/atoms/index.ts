3e96af54d518de65092a278546cfae7813a79196
Jose Daniel Arreaza <jdaniell.93@gmail.com>
2025-06-22 21:16:51 -0300

    feat: re-export Spinner component in atoms index file ✨

--- src/ui/atoms/index.ts
+++ src/ui/atoms/index.ts
@@ -1,4 +1,5 @@
 export { default as Avatar } from "./Avatar";
 export { default as Button } from "./Button";
 export { default as Heading } from "./Heading";
+export { default as Spinner } from "./Spinner";
 export { default as Text } from "./Text";
