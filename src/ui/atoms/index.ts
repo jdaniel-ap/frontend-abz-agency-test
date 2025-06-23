6090797ed29a95779e33e7ee7c4c690cae69da5e
Jose Daniel Arreaza <jdaniell.93@gmail.com>
2025-06-22 22:59:04 -0300

    feat: export FileInput and RadioButton components in index file ✨

--- src/ui/atoms/index.ts
+++ src/ui/atoms/index.ts
@@ -1,5 +1,7 @@
 export { default as Avatar } from "./Avatar";
 export { default as Button } from "./Button";
+export { default as FileInput } from "./FileInput";
 export { default as Heading } from "./Heading";
+export { default as RadioButton } from "./RadioButton";
 export { default as Spinner } from "./Spinner";
 export { default as Text } from "./Text";
